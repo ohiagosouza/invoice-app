047b18af2c881beeaa8739df8f1a7ad50d433973
ohiagosouza <ohiagossouza@gmail.com>
2025-06-04 15:05:47 -0300

    initial commit

---
+++ src/test/java/com/hiagosouza/invoiceapp/InvoiceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiagosouza.invoiceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
