4c17c56b4bd409d838a0060ded20c2cca33a3819
ohiagosouza <ohiagossouza@gmail.com>
2025-06-15 19:26:32 -0300

    deleted AuthLoginRepository and AuthLoginService

--- src/main/java/com/hiagosouza/api/quoted/services/AuthLoginService.java
+++
@@ -1,9 +0,0 @@
-package com.hiagosouza.api.quoted.services;
-
-import com.hiagosouza.api.quoted.model.AuthRequest;
-import com.hiagosouza.api.quoted.model.AuthResponse;
-
-public interface AuthLoginService {
-    AuthRequest loginAuthentication(String email, String password);
-    AuthResponse getLoginAuthentication();
-}
