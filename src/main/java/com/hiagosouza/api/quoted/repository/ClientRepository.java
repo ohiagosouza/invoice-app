9b402a87b50e6cb5d73f33962af05bbb18141ed4
ohiagosouza <ohiagossouza@gmail.com>
2025-06-16 15:43:21 -0300

    feat: refactor UserController for cleaner user registration and profile retrieval Simplified user creation logic using UserMapper and enums for roles Added endpoint to retrieve authenticated user profile

--- src/main/java/com/hiagosouza/api/quoted/repository/ClientRepository.java
+++ src/main/java/com/hiagosouza/api/quoted/repository/ClientRepository.java
@@ -3,11 +3,12 @@ package com.hiagosouza.api.quoted.repository;
 import com.hiagosouza.api.quoted.model.ClientModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ClientRepository extends MongoRepository<ClientModel, UUID> {
     Optional<ClientModel> findByEmail(String email);
     Optional<ClientModel> findByDocument(String document);
-    Optional<ClientModel> findByName(String name);
+    List<ClientModel> findClientsByOwnerId(String ownerId);
 }
